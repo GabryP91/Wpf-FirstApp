e0c5fa27333d23f8fd213da3f8dabd9871f78246
GabryP91 <gabry.palma@libero.it>
2025-04-17 18:57:57 +0200

    Aggiungere i file di progetto.

---
+++ Wpf-FirstApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Wpf_FirstApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool variabile = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnRun1_Click(object sender, RoutedEventArgs e)
+        {
+            if (variabile)
+            {
+                tbHello.Text = "Stopped";
+                btnRun1.Content = "Run";
+            }
+            else
+            {
+                tbHello.Text = "Running";
+                btnRun1.Content = "Stop";
+            }
+
+            variabile = !variabile;
+        }
+    }
+}
